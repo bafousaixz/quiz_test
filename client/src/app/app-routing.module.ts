b6415d04bdb88bbfb4ff738bdfac42d774fd737d
bafousaixz <dongthoigian31313@gmail.com>
2020-07-31 10:37:37 +0700

    profile

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { HomeComponent } from './component/home/home.component';
+import { ErrorComponent } from './component/error/error.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'login', component: LoginComponent}
+  {
+    path: '',
+    component: HomeComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: '**', 
+    component: ErrorComponent
+  },
+  
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { HomeComponent } from './component/home/home.component';
 import { UsersService } from 'src/app/login/users.service';
+import { ErrorComponent } from './component/error/error.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-   
+    ErrorComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
