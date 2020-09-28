0115bb7302d3b51fd7de66379f754cb49a01dc7e
bafousaixz <dongthoigian31313@gmail.com>
2020-09-28 17:41:46 +0700

    fix UI 404 page and end

--- client/src/app/component/error/error.component.ts
+++ client/src/app/component/error/error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-error',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _location: Location,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  goBack(){
+    this._location.back();
   }
 
 }
